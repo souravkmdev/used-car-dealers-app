d9f5dec0c9f08effcb096a85ef5796b95aaa88a2
Suhail <suhailkalyanimotors@gmail.com>
2026-07-01 11:26:45 +0530

    adding status bar translucent for below android 12

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import SplashScreen from './app/screens/auth/SplashScreen';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SizeConfigProvider } from './app/utils/SizeConfig';
+import SplashScreen from './app/screens/auth/SplashScreen';
 
 function App() {
   return (
-    <SizeConfigProvider>
-      <SplashScreen />
-    </SizeConfigProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SizeConfigProvider>
+        <SafeAreaProvider>
+          <StatusBar
+            translucent={true}
+            backgroundColor="transparent"
+            barStyle="dark-content"
+          />
+          <NavigationContainer>
+            <SplashScreen />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </SizeConfigProvider>
+    </GestureHandlerRootView>
   );
 }
+
 export default App;
